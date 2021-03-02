1595dce0811e0ae42a407bed3227f7f979698cf3
DJH <djh1449306350@163.com>
2021-03-02 19:26:59 +0800

    增加tostring

--- src/com/Day/Student.java
+++ src/com/Day/Student.java
@@ -4,4 +4,13 @@ public class Student {
     private Integer id;
     private String name;
     private String email;
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
